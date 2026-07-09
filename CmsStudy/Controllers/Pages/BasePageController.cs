6b9c345245c9b471abab598b22f2614ae90a71ea
grape.tu <grape.tu@esi-tech.net>
2026-07-09 11:09:00 +0800

    Hamu

--- CmsStudy/Controllers/Pages/BasePageController.cs
+++ CmsStudy/Controllers/Pages/BasePageController.cs
@@ -1,15 +1,21 @@
 ﻿using CmsStudy.Models.Pages;
+using CmsStudy.Models.Pages.ViewModels;
 using EPiServer.Core;
 using EPiServer.Web.Mvc;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CmsStudy.Controllers.Pages
 {
-    public class BasePageController<T> : PageController<T> where T : PageData
+    public class BasePageController<T> : PageController<T> where T : AbstractContentPage
     {
         protected IActionResult PageView(T model)
         {
-            return View($"~/Views/Pages/{typeof(T).Name}.cshtml", model);
+            var viewModel = new PageViewModel<T>(model);
+
+            viewModel.PageTitle ??= string.IsNullOrEmpty(model.PageTitle) ? model.Name : model.PageTitle;
+            
+
+            return View($"~/Views/Pages/{typeof(T).Name}.cshtml", viewModel);
         }
     }
 }
---
+++ CmsStudy/Models/Pages/ViewModels/PageViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace CmsStudy.Models.Pages.ViewModels
+{
+    public abstract class PageViewModel
+    {
+        public string? PageTitle { get; set; }
+    }
+
+    public class PageViewModel<T> : PageViewModel
+    {
+        public T Page { get; }
+
+        public PageViewModel(T page)
+        {
+            Page = page;
+        }
+
+    }
+}
